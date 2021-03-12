8768c603aaa254e4946ed59d31f86b64548b95b7
Adnan <adnan_abbasi@yahoo.com>
2021-03-12 18:03:03 -0500

    End of Section 8: Front End User Auth & Profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import { Container } from 'react-bootstrap';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import LoginScreen from './screens/LoginScreen.js';
+import RegisterScreen from './screens/RegisterScreen.js';
+import ProfileScreen from './screens/ProfileScreen.js';
 
 const App = () => {
   return (
@@ -11,7 +13,9 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/login' component={LoginScreen} exact />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
         </Container>
       </main>
       <Footer />
